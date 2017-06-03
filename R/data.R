e254255f902951da7597675006e27654279a9881
Vince Carey <stvjc@channing.harvard.edu>
2017-06-03 18:25:46 -0400

    moved full app code to R folder for improved namespace management

---
+++ R/data.R
@@ -0,0 +1,4 @@
+#' ivySE: SummarizedExperiment for IvyGAP expression data and metadata
+#' @format SummarizedExperiment instance
+#' @source processed from \url{glioblastoma.alleninstitute.org}
+"ivySE"
